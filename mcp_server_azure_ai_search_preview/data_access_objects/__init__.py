0186d616805c6cbb8475c67d2b7083e5fa860290
Israel Ekpo <isekpo@microsoft.com>
2025-05-05 00:19:55 -0400

    saving changes

--- mcp_server_azure_ai_search_preview/data_access_objects/__init__.py
+++ mcp_server_azure_ai_search_preview/data_access_objects/__init__.py
@@ -1,6 +1,6 @@
 
-
-from .dao import SearchIndexDao, SearchBaseDao, SearchClientDao, SearchIndexerDao
+from mcp_server_azure_ai_search_preview.data_access_objects.dao import SearchIndexDao, SearchBaseDao, SearchClientDao, SearchIndexerDao
+# from .dao import SearchIndexDao, SearchBaseDao, SearchClientDao, SearchIndexerDao
 
 __all__ = (
     'SearchBaseDao',
